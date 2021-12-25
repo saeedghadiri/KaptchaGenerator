334bc3d1bc8d0172bced73b89d3dfe1f3529b643
saeedghadiri <saeed.ghadiri@gmail.com>
2021-12-25 19:01:21 +0330

    fix linux path bugs

--- src/main/java/com/saeed/kaptchagen/KaptchaMain.java
+++ src/main/java/com/saeed/kaptchagen/KaptchaMain.java
@@ -11,29 +11,37 @@ import com.google.code.kaptcha.util.Config;
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.google.code.kaptcha.text.impl.DefaultTextCreator;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class KaptchaMain {
     public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
-        String Folder = args[0];
-        String img_width = args[1];
-        String img_height = args[2];
-        String font_size = args[3];
-        String font_color = args[4];
-        String char_string = args[5];
-        String char_length = args[6];
-        int num_samples = Integer.parseInt(args[7]);
-        int add_space = Integer.parseInt(args[8]);
+//        String Folder = args[0];
+//        String img_width = args[1];
+//        String img_height = args[2];
+//        String font_size = args[3];
+//        String font_color = args[4];
+//        String char_string = args[5];
+//        String char_length = args[6];
+//        int num_samples = Integer.parseInt(args[7]);
+//        int add_space = Integer.parseInt(args[8]);
+        
 
-//
-//        String Folder = "data";
-//        String img_width = "200";
-//        String img_height = "50";
-//        String font_size = "50";
-//        String font_color = "0,0,230";
-//        String char_string = "0123456789";
-//        String char_length = "5";
-//        int num_samples = 2000;
-//        int add_space = 22;
+        String Folder = "data";
+        String img_width = "200";
+        String img_height = "50";
+        String font_size = "50";
+        String font_color = "0,0,230";
+        String char_string = "0123456789";
+        String char_length = "5";
+        int num_samples = 2000;
+        int add_space = 22;
+
+        Path currentPath = Paths.get(System.getProperty("user.dir"));
+        Path dataPath = Paths.get(currentPath.toString(), Folder);
+        System.out.println(dataPath.toString());
+        new File(dataPath.toString()).mkdirs();
 
         properties.setProperty("kaptcha.image.width", img_width);
         properties.setProperty("kaptcha.image.height", img_height);
@@ -42,7 +50,7 @@ public class KaptchaMain {
         properties.setProperty("kaptcha.textproducer.char.string", char_string);
         properties.setProperty("kaptcha.textproducer.char.length", char_length);
 
-        new File(Folder).mkdirs();
+
         Config config = new Config(properties);
         
         DefaultKaptcha kaptcha = new DefaultKaptcha();
@@ -58,8 +66,8 @@ public class KaptchaMain {
 
             }
             BufferedImage image = kaptcha.createImage( text_generated);
-
-            File output = new File( Folder + "\\" + text_generated + ".jpg");
+            Path filePath = Paths.get(dataPath.toString(), text_generated + ".jpg");
+            File output = new File(filePath.toString());
             ImageIO.write(image, "jpg", output); 
         }  
 
